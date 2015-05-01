0a9dd8d73de66706fe4bec577b807f7c25605cb7
Damien Quarrington <d.quarrington@gmail.com>
2015-05-01 17:52:50 +0100

    Missed class from previous check in

---
+++ DesignPatterns.Strategy/Model/AggressiveStance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DesignPatterns.Strategy.Model {
+    public class AggressiveStance : IStance {
+
+        public string Attack(string name) {
+            return string.Format("{0} attacks ferociously!", name);
+        }
+
+        public string Move(string name) {
+            return string.Format("{0} move fast and aggressively!", name);
+        }
+
+        public string Defend(string name) {
+            return string.Format("{0} attacks instead of defending!", name);
+        }
+
+        public string StanceName() {
+            return "Aggressive";
+        }
+    }
+}
