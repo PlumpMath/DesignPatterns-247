7731540061e25e69d86447d5cd50e4b0017f9377
projectBlue <d.quarrington@gmail.com>
2016-02-29 19:18:05 +0000

    Test

---
+++ DesignPatterns.Strategy/Model/Robot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Strategy.Model
+{
+    public class Robot
+    {
+        public string Name { get; set; }
+        
+        public IStance Stance { get; set; }
+
+        public Robot(string name, IStance stance)
+        {
+            this.Name = name;
+            this.Stance = stance;
+        }
+
+        public string Attack()
+        {
+            return this.Stance.Attack(this.Name);
+        }
+
+        public string Defend()
+        {
+            return this.Stance.Defend(this.Name);
+        }
+
+        public string Move()
+        {
+            return this.Stance.Move(this.Name);
+        }
+    }
+}
